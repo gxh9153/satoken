22f369cc3d159e3384d45ac4ed0af6eb14cea655
gxh <1320546563@qq.com>
2021-09-09 16:10:26 +0800

    Initial commit

---
+++ src/main/java/com/gxh/satoken/SatokenApplication.java
@@ -0,0 +1,13 @@
+package com.gxh.satoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SatokenApplication.class, args);
+    }
+
+}
